9763ae2df64aa82f09a2fbbaf382a5724f63c433
yelyzavetalubenets <el1111zavetaaaaa@gmail.com>
2026-06-21 20:12:20 +0300

    resolve the supermarket queue task, remove unused imports from two sum

--- src/main/java/algorithm/challenge/faang_practice/arrays/TwoSum.java
+++ src/main/java/algorithm/challenge/faang_practice/arrays/TwoSum.java
@@ -3,7 +3,9 @@ package algorithm.challenge.faang_practice.arrays;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TwoSum {
     /**
